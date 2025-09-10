ff73407cd2c0f4be6516fa99270b244888247e22
AYMENE-K09 <khalidaymene09@gmail.com>
2025-09-10 13:42:00 +0100

    Create exercies3.js

---
+++ exercies3.js
@@ -0,0 +1,7 @@
+const the_main_name = "aymene";
+let user_name = prompt("enter your name: ");
+if (the_main_name === user_name) {
+    console.log("are we brothers or what?");
+} else {
+    console.log("done");
+}
