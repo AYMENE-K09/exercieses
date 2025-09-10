f7785724741726987417cd0149e5f622a6f95c83
AYMENE-K09 <khalidaymene09@gmail.com>
2025-09-10 13:33:09 +0100

    Create exercise4.js

---
+++ exercise4.js
@@ -0,0 +1,7 @@
+const the_needed_height = 145;
+let user_height = prompt("enter your height by cm: ");
+if (the_needed_height < user_height) {
+    console.log("you are tall enough to ride");
+} else {
+    console.log("you need grow some more to ride");
+}
