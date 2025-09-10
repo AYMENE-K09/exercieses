e73b7ba57fd72842d9d2c3d5c5786f0ed7028d4c
AYMENE-K09 <khalidaymene09@gmail.com>
2025-09-10 13:38:21 +0100

    Update exercise4.js

--- exercise4.js
+++ exercise4.js
@@ -1,5 +1,5 @@
 const the_needed_height = 145;
-let user_height = prompt("enter your height by cm: ");
+let user_height = ParseInt(prompt("enter your height by cm: "));
 if (the_needed_height < user_height) {
     console.log("you are tall enough to ride");
 } else {
